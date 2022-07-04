7aff38f620835ebe88c21f67eb34e6ccd725b541
Andrey <41819110+Vonamugan@users.noreply.github.com>
2022-07-04 17:46:44 +0200

    figures timer and lostfigureslists :wrench:

---
+++ src/models/figures/King.ts
@@ -0,0 +1,19 @@
+import {Figure, FigureNames} from "./Figure";
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+import blackLogo from "../../assets/b_king.png";
+import whiteLogo from "../../assets/w_king.png";
+
+export class King extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+    this.name = FigureNames.KING;
+  }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target))
+      return false;
+    return true
+  }
+}
